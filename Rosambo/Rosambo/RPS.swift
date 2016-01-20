ab73378edfe81c94ce90923d7ce8c49cb3c4239f
woogii <jhsw0201@naver.com>
2016-01-20 20:11:31 +0900

    feat : Implement enum that represents roshambo

---
+++ Rosambo/Rosambo/RPS.swift
@@ -0,0 +1,64 @@
+//
+//  RPS.swift
+//  Rosambo
+//
+//  Created by Hyun on 2016. 1. 20..
+//  Copyright © 2016년 wook2. All rights reserved.
+//
+
+import Foundation
+
+
+enum RPS {
+    
+    case Rock, Paper, Scissors
+    
+    
+    init() {
+        
+        // Randomly generate the number between 0 and 2
+        switch arc4random() % 3 {
+            
+        case 0:
+            self = .Rock
+        case 1:
+            self = .Paper
+        default:
+            self = .Scissors
+        }
+    }
+    
+    func defeats(opponent: RPS)->Bool {
+        
+        switch(self, opponent) {
+            
+            case(.Paper, .Rock), (.Scissors, .Paper), (.Rock, .Scissors):
+                return true;
+            default:
+                return false;
+        }
+    }
+    
+}
+
+// Conform to CustomStringConvertible protocol 
+
+extension RPS: CustomStringConvertible {
+    
+    var description: String {
+        
+        get {
+            
+            switch(self) {
+            
+            case .Rock:
+                return "Rock"
+            case .Scissors:
+                return "Scissor"
+            case .Paper:
+                return "Paper"
+            }
+            
+        }
+    }
+}
---
+++ Rosambo/Rosambo/RPSMatch.swift
@@ -0,0 +1,9 @@
+//
+//  RPSMatch.swift
+//  Rosambo
+//
+//  Created by Hyun on 2016. 1. 20..
+//  Copyright © 2016년 wook2. All rights reserved.
+//
+
+import Foundation
