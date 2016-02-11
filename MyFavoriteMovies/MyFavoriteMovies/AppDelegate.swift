2cfd60ecaa39901ad7d202c1f4da33fddea40bc2
woogii <jhsw0201@naver.com>
2016-02-11 22:16:17 +0900

    chore : Modify gitignore file which is intended to hide API Key in this repository

--- MyFavoriteMovies/MyFavoriteMovies/AppDelegate.swift
+++ MyFavoriteMovies/MyFavoriteMovies/AppDelegate.swift
@@ -2,67 +2,45 @@
 //  AppDelegate.swift
 //  MyFavoriteMovies
 //
-//  Created by Jarrod Parkes on 1/23/15.
-//  Copyright (c) 2015 Udacity. All rights reserved.
+//  Created by Hyun on 2016. 2. 11..
+//  Copyright © 2016년 wook2. All rights reserved.
 //
 
 import UIKit
 
-// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    // MARK: Properties
-    
+
     var window: UIWindow?
-    
-    /* Constants for TheMovieDB */
-    let apiKey = "12316a351a060d40e3793632edc8e41b"
-    let baseURLString = "http://api.themoviedb.org/3/"
-    let baseURLSecureString = "https://api.themoviedb.org/3/"
-    
-    /* Need these for login */
-    var requestToken: String? = nil
-    var sessionID: String? = nil
-    var userID: Int? = nil
-    
-    /* Configuration for TheMovieDB, we'll take care of this for you =)... */
-    var config = Config()
-    
-    // MARK: UIApplicationDelegate
-    
+
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
-        /* If necessary, update the configuration */
-        config.updateIfDaysSinceUpdateExceeds(7)
-        
+        // Override point for customization after application launch.
         return true
     }
-}
 
-// MARK: - Escape HTML Parameters
-
-extension AppDelegate {
-    
-    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
-    func escapedParameters(parameters: [String : AnyObject]) -> String {
-        
-        var urlVars = [String]()
-        
-        for (key, value) in parameters {
-            
-            /* Make sure that it is a string value */
-            let stringValue = "\(value)"
-            
-            /* Escape it */
-            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
-            
-            /* Append it */
-            urlVars += [key + "=" + "\(escapedValue!)"]
-            
-        }
-                        
-        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
 }
+
---
+++ MyFavoriteMovies/Secret.swift
@@ -0,0 +1,9 @@
+//
+//  Secret.swift
+//  MyFavoriteMovies
+//
+//  Created by Hyun on 2016. 2. 11..
+//  Copyright © 2016년 wook2. All rights reserved.
+//
+
+import Foundation
