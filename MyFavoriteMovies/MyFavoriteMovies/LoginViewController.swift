627ceed652fdc8fde12e8d9406d32f57638fe6fb
woogii <jhsw0201@naver.com>
2016-02-12 17:26:47 +0900

    feat & chore : Configure the layout of LoginViewController                Copy images into project files

---
+++ MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Hyun on 2016. 2. 12..
+//  Copyright © 2016년 wook2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
