9400c46ad3ffe7b80992d62135a05c21cbc56c0a
woogii <jhsw0201@naver.com>
2016-02-16 15:58:38 +0900

    feat : Implement functions associated with keyboard notification and textfield events

--- MyFavoriteMovies/MyFavoriteMovies/AppDelegate.swift
+++ MyFavoriteMovies/MyFavoriteMovies/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     
-
+    var session : NSURLSession {
+        
+        return NSURLSession.sharedSession()
+        
+    }
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
--- MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
+++ MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
@@ -11,18 +11,79 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    var appDelegate : AppDelegate!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginResponse: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+
         // Do any additional setup after loading the view, typically from a nib.
-        // let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        subscribeNotification(TMDBConstants.Selectors.KeyboardWillShow, notification: UIKeyboardWillShowNotification )
+        subscribeNotification(TMDBConstants.Selectors.KeyboardWillHide, notification: UIKeyboardWillHideNotification);
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTapGesture" )
+        
+        self.view.addGestureRecognizer(tapGesture)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func handleTapGesture() {
+        // Cause the view to resign the first responder status
+        view.endEditing(true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        unsubsribeNotification(UIKeyboardWillShowNotification)
+        unsubsribeNotification(UIKeyboardWillHideNotification)
     }
     
     
+    @IBAction func loginButtonClicked(sender: UIButton) {
+        
+    
+    }
+        
+}
+
+extension LoginViewController : UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func keyboardWillShow(notification:NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+            view.frame.origin.y = -keyboardSize.height
+        }
+    }
+    
+    func keyboardWillHide(notification:NSNotification) {
+        view.frame.origin.y = 0
+    }
+    
+    
+   
+}
+
+extension LoginViewController {
+
+    func subscribeNotification(selector:Selector, notification : String) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubsribeNotification(notification : String) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: notification, object: nil)
+    }
 }
