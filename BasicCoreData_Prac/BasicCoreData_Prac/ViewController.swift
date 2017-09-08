9dc242397fa96fa6ee169d2c324d7e360c2a089e
woogii <jhsw0201@naver.com>
2017-09-08 13:11:51 +0200

    feat : Add the UIAlertController and Configure TableView

--- BasicCoreData_Prac/BasicCoreData_Prac/ViewController.swift
+++ BasicCoreData_Prac/BasicCoreData_Prac/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var tableView: UITableView!
-  var item = [String]()
+  var itemList = [String]()
   
   override func viewDidLoad() {
     
@@ -19,29 +19,58 @@ class ViewController: UIViewController {
     
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     tableView.dataSource = self
+    
+    navigationItem.title = "CoreData Practice"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(buttonTapped(_:)))
+    
   }
-
+  
+  func buttonTapped(_ sender:Any) {
+    
+    let alertController = UIAlertController(title: "", message: "Type an Item", preferredStyle: .alert)
+  
+    let okAction = UIAlertAction(title: "OK", style: .default) { (okAction) in
+     
+      guard let textField = alertController.textFields?.first, let text = textField.text else {
+        return
+      }
+    
+      self.itemList.append(text)
+      
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+    
+    alertController.addAction(okAction)
+    alertController.addAction(cancelAction)
+    
+    alertController.addTextField { (textField) in
+      textField.placeholder = "Enter your item"
+    }
+    
+    present(alertController, animated: true, completion: nil)
+    
+  }
+  
 
 
 }
 
 extension ViewController : UITableViewDataSource {
   
-  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-    return item.count
+    return itemList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = ""
-    
+    cell.textLabel?.text = itemList[indexPath.row]
     return cell
   }
   
-  
-  
+
 }
 
